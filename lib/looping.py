7531caaba63f779bf720642ddaee22207801c4b1
leon <onditileon21@gmail.com>
2024-03-01 16:45:32 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # squares = [abs(num) ** 2 for num in int_list]
+    squares = list()
+    for num in int_list:
+        square = num **2
+        squares.append(square)
+    return(squares)
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
+            i += 1
+        elif i % 3 == 0:
+            print("Fizz")
+            i += 1
+        elif i % 5 == 0:
+            print("Buzz")
+            i += 1
+        else:
+            print(i)
+            i += 1
+print()   
